c10ee7d7d7dc67d36f8efc8e2190e7d0836555e3
zwhahn <117476820+zwhahn@users.noreply.github.com>
2024-06-17 15:18:27 -0700

    Add stop_code

--- main.py
+++ main.py
@@ -4,7 +4,10 @@ import config
 # API Parameters
 api_key = config.api_key
 agency = 'AC'
-url = f'http://api.511.org/transit/StopMonitoring?api_key={api_key}&agency={agency}'
+stop_code = '14623'
+
+
+url = f'http://api.511.org/transit/StopMonitoring?api_key={api_key}&agency={agency}&stopCode={stop_code}'
 
 response = requests.get(url)
 status = response.status_code
@@ -12,4 +15,6 @@ if status == 200:
     print("Succesful Request")
 else:
     print(f'Request Fail: {status}')
+
+print(response)
 
