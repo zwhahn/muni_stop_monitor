d9b49f94650a9f12326f73dae31d99bde11b0717
zwhahn <117476820+zwhahn@users.noreply.github.com>
2024-06-21 12:29:52 -0700

    Print expected arrival times

--- main.py
+++ main.py
@@ -22,7 +22,44 @@ headers = response.headers
 
 # JSON formatted resopnse, method from: https://groups.google.com/g/511sfbaydeveloperresources/c/K40TiNf8ydA
 response_json = json.loads(codecs.decode(response.content, encoding='utf-8-sig'))
+# print(response_json)
 
-print(response_json['ServiceDelivery']['StopMonitoringDelivery']['MonitoredStopVisit'])
+for item in response_json:
+    # print("item: ", item)
+    
+    for subitem in response_json[item]:
+        # print("subitem: ", subitem)
 
+        # Check request status- Indicates success or failure of request.
+        if subitem == "Status":
+            if response_json[item][subitem] == True:
+                print("Status is True")
+            else:
+                print("Request Failed")
+        
+        if subitem == "StopMonitoringDelivery":
+            print("Entered StopMonitoringDelivery")
+            for subsubitem in response_json[item][subitem]:
+                # print("subsub: ", subsubitem)
+                pass
+            if subsubitem == 'MonitoredStopVisit':
+                # print("Entered MonitoredStopVisit")
+                for subsubsubitem in response_json[item][subitem][subsubitem]:
+                    # print("subsubsubitem:", subsubsubitem)
+                    for foursub in response_json[item][subitem][subsubitem][0]:
+                        # print("foursub: ", foursub)
+                        if foursub == "MonitoredVehicleJourney":
+                            # print("MonitoredVehicleJourney:", response_json[item][subitem][subsubitem][0]['MonitoredVehicleJourney'])
+
+                            #Check that trip is monitiored
+                            # print(response_json[item][subitem][subsubitem][0]['MonitoredVehicleJourney']['Monitored'])
+
+                            # Data for the stop
+                            # print(response_json[item][subitem][subsubitem][0]['MonitoredVehicleJourney']['MonitoredCall'])
+
+                            print("ExpectedArrivalTime: ", response_json[item][subitem][subsubitem][0]['MonitoredVehicleJourney']['MonitoredCall']['ExpectedArrivalTime'])
+                            print("Type ExpectedArrivalTime: ", type(response_json[item][subitem][subsubitem][0]['MonitoredVehicleJourney']['MonitoredCall']['ExpectedArrivalTime']))
+
+
+                            
 
