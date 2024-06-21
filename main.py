c0d43de3168bce5c22e68ea3f4d12eded7735b14
zwhahn <117476820+zwhahn@users.noreply.github.com>
2024-06-21 15:42:12 -0700

    Rename variables

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import codecs
 import json
 from datetime import datetime
 import pytz
+import pandas
 
 # Perform API Request
 def api_request(api_key=config.api_key, agency=config.agency, stop_code=config.stop_code):
@@ -26,61 +27,58 @@ def get_bus_info(response_json):
     count = 0
     bus_info = {"bus1":{}, "bus2":{}, "bus3":{}}
 
-    for item in response_json:
-        # print("item: ", item)
-        
-        for subitem in response_json[item]:
-            # print("subitem: ", response_json[item][subitem])
+    for content in response_json:
+        print("content", content)
+        for header in response_json[content]:
 
             # Check request status- Indicates success or failure of request.
-            if subitem == "Status":
-                if response_json[item][subitem] == True:
+            if header == "Status":
+                if response_json[content][header] == True:
                     print("Status is True")
                 else:
                     print("Request Failed")
             
-            if subitem == "StopMonitoringDelivery":
+            if header == "StopMonitoringDelivery":
                 # print("Entered StopMonitoringDelivery")
-                for subsubitem in response_json[item][subitem]:
-                    # print("subsub: ", response_json[item][subitem][subsubitem])
-                    pass
-                if subsubitem == 'MonitoredStopVisit':
-                    # print("Entered MonitoredStopVisit")
-                    for subsubsubitem in response_json[item][subitem][subsubitem]:
-                        # print("subsubsubitem type:", type(subsubsubitem))
-                        for foursub in subsubsubitem['MonitoredVehicleJourney']:
-                            if foursub == 'Monitored':
-                                monitored_status = subsubsubitem['MonitoredVehicleJourney']['Monitored']
-                                if count == 0:
-                                    bus_info['bus1']['status'] = monitored_status
-                                if count == 1:
-                                    bus_info['bus2']['status'] = monitored_status
-                                if count == 2:
-                                    bus_info['bus3']['status'] = monitored_status
-                    
-                            # Data for the stop
-                            if foursub == 'MonitoredCall':
-                                # Convert Expected Arrival Time from string to DateTime object 
-                                expected_arrival_time_str_UTC = subsubsubitem['MonitoredVehicleJourney']['MonitoredCall']['ExpectedArrivalTime']
-                                expected_arrival_time_UTC = datetime.strptime(expected_arrival_time_str_UTC, '%Y-%m-%dT%H:%M:%SZ')
+                for StopMonitoringDelivery_var in response_json[content][header]:
+                    # print("StopMonitoringDelivery_var: ", StopMonitoringDelivery_var)
+                    if StopMonitoringDelivery_var == 'MonitoredStopVisit':
+                        # print("Entered MonitoredStopVisit")
+                        for MonitoredStopVisit_var in response_json[content][header][StopMonitoringDelivery_var]:
+                            for MonitoredVehicleJourney_var in MonitoredStopVisit_var['MonitoredVehicleJourney']:
+                                if MonitoredVehicleJourney_var == 'Monitored':
+                                    monitored_status = MonitoredStopVisit_var['MonitoredVehicleJourney']['Monitored']
+                                    if count == 0:
+                                        bus_info['bus1']['status'] = monitored_status
+                                    if count == 1:
+                                        bus_info['bus2']['status'] = monitored_status
+                                    if count == 2:
+                                        bus_info['bus3']['status'] = monitored_status
+                        
+                                # Data for the stop
+                                if MonitoredVehicleJourney_var == 'MonitoredCall':
+                                    # Convert Expected Arrival Time from string to DateTime object 
+                                    expected_arrival_time_str_UTC = MonitoredStopVisit_var['MonitoredVehicleJourney']['MonitoredCall']['ExpectedArrivalTime']
+                                    expected_arrival_time_UTC = datetime.strptime(expected_arrival_time_str_UTC, '%Y-%m-%dT%H:%M:%SZ')
 
-                                # Correct timezone (UTC to PT)
-                                original_timezone = pytz.timezone(config.original_timezone)
-                                target_timezone = pytz.timezone(config.target_timezone)
+                                    # Correct timezone (UTC to PT)
+                                    original_timezone = pytz.timezone(config.original_timezone)
+                                    target_timezone = pytz.timezone(config.target_timezone)
 
-                                expected_arrival_time_UTC = original_timezone.localize(expected_arrival_time_UTC)  # Assign timezone to exisisting time
-                                expected_arrival_time_PT = expected_arrival_time_UTC.astimezone(target_timezone)
-                            
-                                if count == 0:
-                                    bus_info['bus1']['predicted_arrival_time'] = expected_arrival_time_PT
-                                if count == 1:
-                                    bus_info['bus2']['predicted_arrival_time'] = expected_arrival_time_PT
-                                if count == 2:
-                                    bus_info['bus3']['predicted_arrival_time'] = expected_arrival_time_PT
-                            
-                                count = count + 1
+                                    expected_arrival_time_UTC = original_timezone.localize(expected_arrival_time_UTC)  # Assign timezone to exisisting time
+                                    expected_arrival_time_PT = expected_arrival_time_UTC.astimezone(target_timezone)
+                                
+                                    if count == 0:
+                                        bus_info['bus1']['predicted_arrival_time'] = expected_arrival_time_PT
+                                    if count == 1:
+                                        bus_info['bus2']['predicted_arrival_time'] = expected_arrival_time_PT
+                                    if count == 2:
+                                        bus_info['bus3']['predicted_arrival_time'] = expected_arrival_time_PT
+                                
+                                    count = count + 1
                                     
     return bus_info
 
 response_json = api_request()
-print(get_bus_info(response_json))
+bus_info = get_bus_info(response_json)
+print(bus_info)
