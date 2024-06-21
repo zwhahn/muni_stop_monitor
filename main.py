bd800b0f589f50c153c33819d6992004e874f2d5
zwhahn <117476820+zwhahn@users.noreply.github.com>
2024-06-21 15:02:29 -0700

    Create bus_info dictionary

--- main.py
+++ main.py
@@ -5,11 +5,6 @@ import json
 from datetime import datetime
 import pytz
 
-# API Parameters from config file
-# api_key = config.api_key
-# agency = config.agency
-# stop_code = config.stop_code
-
 # Perform API Request
 def api_request(api_key=config.api_key, agency=config.agency, stop_code=config.stop_code):
     url = f'http://api.511.org/transit/StopMonitoring?api_key={api_key}&agency={agency}&stopCode={stop_code}&format=JSON'
@@ -28,6 +23,9 @@ def api_request(api_key=config.api_key, agency=config.agency, stop_code=config.s
 
 # Extract needed information from JSON and reate bus_info dictionary
 def get_bus_info(response_json):
+    count = 0
+    bus_info = {"bus1":{}, "bus2":{}, "bus3":{}}
+
     for item in response_json:
         # print("item: ", item)
         
@@ -53,24 +51,36 @@ def get_bus_info(response_json):
                         for foursub in subsubsubitem['MonitoredVehicleJourney']:
                             if foursub == 'Monitored':
                                 monitored_status = subsubsubitem['MonitoredVehicleJourney']['Monitored']
-                                print("Monitor Status: ", monitored_status)
-
+                                if count == 0:
+                                    bus_info['bus1']['status'] = monitored_status
+                                if count == 1:
+                                    bus_info['bus2']['status'] = monitored_status
+                                if count == 2:
+                                    bus_info['bus3']['status'] = monitored_status
+                    
                             # Data for the stop
                             if foursub == 'MonitoredCall':
-
                                 # Convert Expected Arrival Time from string to DateTime object 
                                 expected_arrival_time_str_UTC = subsubsubitem['MonitoredVehicleJourney']['MonitoredCall']['ExpectedArrivalTime']
                                 expected_arrival_time_UTC = datetime.strptime(expected_arrival_time_str_UTC, '%Y-%m-%dT%H:%M:%SZ')
 
                                 # Correct timezone (UTC to PT)
-                                original_timezone = pytz.timezone('UTC')
-                                target_timezone = pytz.timezone('US/Pacific')
+                                original_timezone = pytz.timezone(config.original_timezone)
+                                target_timezone = pytz.timezone(config.target_timezone)
 
-                                expected_arrival_time_UTC = original_timezone.localize(expected_arrival_time_UTC)
+                                expected_arrival_time_UTC = original_timezone.localize(expected_arrival_time_UTC)  # Assign timezone to exisisting time
                                 expected_arrival_time_PT = expected_arrival_time_UTC.astimezone(target_timezone)
-
-
-                                print("ExpectedArrivalTime: ", expected_arrival_time_PT)
+                            
+                                if count == 0:
+                                    bus_info['bus1']['predicted_arrival_time'] = expected_arrival_time_PT
+                                if count == 1:
+                                    bus_info['bus2']['predicted_arrival_time'] = expected_arrival_time_PT
+                                if count == 2:
+                                    bus_info['bus3']['predicted_arrival_time'] = expected_arrival_time_PT
+                            
+                                count = count + 1
+                                    
+    return bus_info
 
 response_json = api_request()
 print(get_bus_info(response_json))
